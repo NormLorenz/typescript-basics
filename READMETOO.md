09ce989d787a7de9a46a6ff117b226cea7dda355
norm <normlorenz@gmail.com>
2018-06-28 13:45:44 -0700

    working my way through

--- READMETOO.md
+++ READMETOO.md
@@ -1,4 +1,8 @@
-## https://mail.google.com/mail/u/0/#inbox/1643e0e79d24a9ab
+## https://ultimateangular.teachable.com/courses/enrolled/210176
 
 * pure verses impure function
 * void is used for an impure function
+
+## function types
+
+* 
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,74 @@
-const pizzaCost: number = 10;
-const pizzaToppings: number = 5;
 
-function calculatePrice(cost: number, toppings: number): number {
-  return cost + 1.5 * toppings;
+// typed function declaration
+let sumOrder: (price: number, quantity: number) => number;
+sumOrder = (x, y) => x * y;
+const sum = sumOrder(25, 2);
+console.log(`total sum: ${sum}`);
+
+// optional typed function declaration
+let sumOrder2: (price: number, quantity?: number) => number;
+sumOrder2 = (x, y = 1) => x * y;
+const sum2 = sumOrder2(25);
+
+// working with objects
+let pizza: { name: string, price: number, getName(): string } = {
+  name: 'plain old pepperoni',
+  price: 20,
+  getName() {
+    return pizza.name;
+  }
+};
+console.log(pizza.getName());
+
+// arrays
+const sizes: string[] = ['small', 'medium', 'large'];
+
+// generics
+let toppings: string[];
+toppings = ['pepperoni', 'tomato', 'bacon'];
+
+let cars: Array<string>;
+cars = ['chevrolet', 'ford', 'hudson'];
+
+// tuples
+let tool: [string, number, boolean];
+tool = ['wrench', 20, true];
+
+// type aliases
+type Size = 'small' | 'medium' | 'large';
+type Callback = (size: Size) => void;
+let pizzaSize: Size = 'small';
+const selectSize: Callback = (x) => {
+  pizzaSize = x;
+}
+selectSize('small');
+
+// type assertions
+type Animal = { name: string, hasFur: boolean, legs: number };
+const animal: Animal = { name: 'cat', hasFur: true, legs: 4 };
+const serialized = JSON.stringify(animal);
+
+function getNameFromJSON(obj: string) {
+  return (JSON.parse(obj) as Animal).name;
 }
 
-const cost: number = calculatePrice(pizzaCost, pizzaToppings);
-console.log(`pizza costs: ${cost}`);
+getNameFromJSON(serialized);
 
-const coupon: string = 'pizza25';
+// interfaces
+interface Truck {
+  name: string;
+  sizes: string[];
+};
 
-function normalizeCoupon(code: string): string {
-  return code.toUpperCase();
+interface Trucks {
+  data: Truck
 }
 
-const couponMessage: string = `final coupon is ${normalizeCoupon(coupon)}`;
-console.log(couponMessage);
+function createTruck(name: string, sizes: string[]): Truck {
+  return {
+    name,
+    sizes,
+  }
+}
+
+const truck = createTruck('Dodge', ['small', 'medium']);
