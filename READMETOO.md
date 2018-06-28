f180597de406597df1dec5504c85b570be5e5100
norm <normlorenz@gmail.com>
2018-06-28 08:03:25 -0700

    ready to publish to github

--- READMETOO.md
+++ READMETOO.md
@@ -1,2 +1,4 @@
 ## https://mail.google.com/mail/u/0/#inbox/1643e0e79d24a9ab
 
+* pure verses impure function
+* void is used for an impure function
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Ahoy TypeScript!');
+const pizzaCost: number = 10;
+const pizzaToppings: number = 5;
+
+function calculatePrice(cost: number, toppings: number): number {
+  return cost + 1.5 * toppings;
+}
+
+const cost: number = calculatePrice(pizzaCost, pizzaToppings);
+console.log(`pizza costs: ${cost}`);
+
+const coupon: string = 'pizza25';
+
+function normalizeCoupon(code: string): string {
+  return code.toUpperCase();
+}
+
+const couponMessage: string = `final coupon is ${normalizeCoupon(coupon)}`;
+console.log(couponMessage);
